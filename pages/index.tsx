ef2e658a3213407b0261fae28c8e263ec4a368cb
= <albin.groen@demando.se>
2023-02-22 20:18:54 +0100

    Add audio player on each episode

--- pages/index.tsx
+++ pages/index.tsx
@@ -158,27 +158,40 @@ export default function Home({
               {podcast.items.map((episode) => (
                 <li key={episode.guid}>
                   <a
+                    target="_blank"
                     href={episode.link}
-                    className="block rounded-lg p-5 -mx-5 hover:bg-gray-100 transition"
+                    rel="noopener noreferrer"
+                    className="block rounded-lg p-5 -mx-5 transition hover:bg-gray-50"
                   >
                     <Stack direction="vertical" spacing="small">
                       <h4 className="text-2xl font-semibold">
                         {episode.title}
                       </h4>
 
-                      <Stack direction="vertical" spacing="large">
-                        {episode.isoDate && (
-                          <p className="text-sm text-[#BA4021] leading-relaxed">
-                            {format(
-                              new Date(episode.isoDate),
-                              "EEEE, MMMM do, yyyy"
-                            )}
-                          </p>
-                        )}
+                      <Stack direction="vertical" spacing="huge">
+                        <Stack direction="vertical" spacing="large">
+                          {episode.isoDate && (
+                            <p className="text-sm text-[#BA4021] leading-relaxed">
+                              {format(
+                                new Date(episode.isoDate),
+                                "EEEE, MMMM do, yyyy"
+                              )}
+                            </p>
+                          )}
 
-                        <p className="leading-relaxed">
-                          {episode.contentSnippet}.
-                        </p>
+                          <p className="leading-relaxed">
+                            {episode.contentSnippet}.
+                          </p>
+                        </Stack>
+
+                        {episode.enclosure?.url && episode.enclosure?.type && (
+                          <audio controls className="w-full">
+                            <source
+                              src={episode.enclosure.url}
+                              type={episode.enclosure.type}
+                            />
+                          </audio>
+                        )}
                       </Stack>
                     </Stack>
                   </a>
