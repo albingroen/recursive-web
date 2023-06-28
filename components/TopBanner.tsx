7cc7d0fac8ebb9fc69259e1ea37c741514bb8aac
Robbert Tuerlings <robberttg@protonmail.com>
2023-06-28 09:39:07 +0200

    feat: Create top banner with Summer message

---
+++ components/TopBanner.tsx
@@ -0,0 +1,22 @@
+import Stack from "./Stack";
+
+const HEADER = "Swedish Midsummer Break";
+const SUBTITLE = "We are enjoying a little summer break and will be back soon!";
+
+export default function TopBanner() {
+  return (
+    <div className="sticky top-0 left-0 right-0 p-2 bg-orange-400 dark:bg-orange-600/90 z-50">
+      <Stack
+        wrap
+        direction="vertical"
+        spacing="none"
+        justify="center"
+        align="center"
+        className="text-center"
+      >
+        <p className="font-semibold">{HEADER}</p>
+        <p className="text-sm">{SUBTITLE}</p>
+      </Stack>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import TopBanner from "@/components/TopBanner";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
+      <TopBanner />
       <Component {...pageProps} />
       <Analytics />
     </main>
